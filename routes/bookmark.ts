2f9a3836a7cb00e95f3fb9bcc1d01a954c264d53
Nikita Nakropin <nikita@nakropin.com>
2024-07-16 11:58:49 +0200

    added functionality to save onto the database. refactored code a little

--- routes/bookmark.ts
+++ routes/bookmark.ts
@@ -3,54 +3,58 @@ import fs from "fs";
 
 type User = {
   uuid: string;
-  bookmarks: string[]
+  bookmarks: number[]
 };
 
 type DB = {
   users: User[];
 };
 
-function getDB() {
+function getDB(): DB {
   const dbFile = fs.readFileSync("./db.json", { encoding: "utf-8" });
   return JSON.parse(dbFile) as DB;
 }
 
+
+
 export const bookmarkRouter = Router();
 
-// Sen User with bookmarks back
+// Send User with bookmarks back
 bookmarkRouter.get("/:uuid", (req, res) => {
   const db = getDB();
-  const uuid = req.params.uuid
-  const user = db.users.find(u => u.uuid === uuid)
+  const uuid = req.params.uuid;
+  const user = db.users.find(u => u.uuid === uuid);
+  
+  //Return 404 if no user found
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
   res.json(user);
 });
 
 bookmarkRouter.post("/:uuid/:movieId", (req, res) => {
   const db = getDB();
-  const uuid = req.params.uuid
-  const movieId = req.params.movieId
-  const user = db.users.find(u => u.uuid === uuid)
+  const uuid = req.params.uuid;
+  const movieId = Number(req.params.movieId);
+  const userIndex = db.users.findIndex(u => u.uuid === uuid);
 
-  if (!user) {
+  //Return 404 if no user found
+  if (userIndex === -1) {
     return res.status(404).json({ message: "User not found" });
   }
-  const updatedDb = {
-    // All entries from our database
-      ...db,
-    // Plus our updated List of Users together with the newly created one
-      users: [...db.users, newUser],
-    };
-    //Write File
-    fs.writeFileSync("./db.json", JSON.stringify(updatedDb, null, 2));
 
+  const user = db.users[userIndex];
+
+  // Has our user bookmarks array have the movieID?
   if (user.bookmarks.includes(movieId)) {
-    // Entfernt das spezifische movieId aus den Bookmarks
+    // if so, remove it
     user.bookmarks = user.bookmarks.filter(id => id !== movieId);
-    res.status(200).json(Number(user.bookmarks));
   } else {
-    // Fügt das spezifische movieId zu den Bookmarks hinzu
+    // else add it
     user.bookmarks.push(movieId);
-    res.status(202).json(Number(user.bookmarks));
   }
-});
 
+  // Save on DB
+  fs.writeFileSync("./db.json", JSON.stringify(db, null, 2));
+  res.status(200).json({ bookmarks: user.bookmarks });
+});
