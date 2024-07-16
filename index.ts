4e69fc1709a096e8066c63db5f28073e64b0fe5f
ShaQay3Q <shaghay3gh@gmail.com>
2024-07-16 10:20:07 +0200

    bookmars based on id can be found now

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { helloRouter } from "./routes/hello";
 import { usersRouter } from "./routes/users";
+import { bookmarksRouter } from "./routes/bookmarks";
 
 // Initialize application
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 // Define an endpoint
 app.use("/hello", helloRouter);
 app.use("/users", usersRouter);
+app.use("/bookmarks", bookmarksRouter);
 
 // Run server
 app.listen(3000, () => {
