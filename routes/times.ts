1044f0b883eaba69b2300d346166a4789c997cee
Fred <frederik.koenig@posteo.de>
2024-07-22 14:26:11 +0200

    adjusted for prisma

--- routes/times.ts
+++ routes/times.ts
@@ -1,49 +1,50 @@
 import { Router } from "express";
-import fs from "fs";
-import { sql } from "../lib/db";
 import { addDays, eachDayOfInterval, format } from "date-fns";
+import { prisma } from "../lib/db";
+import { access } from "fs";
 
 const SeatsTotal = 44;
-
-type ScreeningFromDB = {
-  id: number;
-  date: string;
-  time: string;
-  booked_seats: string;
-  movie_id: string;
-};
 
 export const timesRouter = Router();
 
 timesRouter.get("/", async (req, res) => {
-  const today = new Date();
+  try {
+    const today = new Date();
 
-  const unformattedDatesNextWeek = eachDayOfInterval({
-    start: new Date(today),
-    end: new Date(addDays(today, 6)),
-  });
+    const unformattedDatesNextWeek = eachDayOfInterval({
+      start: new Date(today),
+      end: new Date(addDays(today, 6)),
+    });
 
-  const datesNextWeek = unformattedDatesNextWeek.map((date) =>
-    format(date, "dd-MM-yyyy")
-  );
+    const datesNextWeek = unformattedDatesNextWeek.map((date) =>
+      format(date, "dd-MM-yyyy")
+    );
 
-  const screenings = await sql<
-    ScreeningFromDB[]
-  >`SELECT * FROM screenings WHERE date IN ${sql(datesNextWeek)}`;
+    const screenings = await prisma.screening.findMany({
+      where: {
+        date: {
+          in: datesNextWeek,
+        },
+      },
+    });
 
-  const availableTimes = screenings.reduce<{ [key: string]: string[] }>(
-    (acc, screening) => {
-      if (!acc[screening.date]) {
-        acc[screening.date] = [];
-      }
+    const availableTimes = screenings.reduce<{ [key: string]: string[] }>(
+      (acc, screening) => {
+        if (!acc[screening.date]) {
+          acc[screening.date] = [];
+        }
 
-      if (screening.booked_seats.split(",").length < SeatsTotal) {
-        acc[screening.date].push(screening.time);
-      }
+        if (screening.booked_seats.split(",").length < SeatsTotal) {
+          acc[screening.date].push(screening.time);
+        }
 
-      return acc;
-    },
-    {}
-  );
-  res.json(availableTimes);
+        return acc;
+      },
+      {}
+    );
+    res.json(availableTimes);
+  } catch (error) {
+    console.error("Error fetching available times:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
